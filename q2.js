4da1991a24467179c34576e011d6166b255c8484
Nehal Patel <124572297+Nihal-patel9@users.noreply.github.com>
2023-06-25 18:14:40 +0530

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,22 @@
+function firstUniqChar(s) {
+  const charCount = {};
+  
+  // Count the frequency of each character
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  
+  // Find the index of the first non-repeating character
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (charCount[char] === 1) {
+      return i;
+    }
+  }
+  
+  return -1; // No non-repeating character found
+}
+console.log(firstUniqChar("leetcode")); // Output: 0
+console.log(firstUniqChar("loveleetcode")); // Output: 2
+console.log(firstUniqChar("aabb")); // Output: -1
